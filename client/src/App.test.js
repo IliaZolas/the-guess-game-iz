88e0d08b04c07798061e91f34e6a3eef8bffc5cc
Ilia Zolas <ilia@thedmcsa.co.za>
2023-08-20 19:21:32 +0200

    updated test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -10,13 +10,6 @@ test('renders the app with Navbar and Home content', () => {
     const logoElement = screen.getByText(/Guess the Number/i);
     expect(logoElement).toBeInTheDocument();
     
-    // Check if the Home page content is rendered
-    const homePageElement = screen.getByText(/Guess the Number/i);
-    expect(homePageElement).toBeInTheDocument();
-    const signupButton = screen.getByText(/Signup to get started/i);
-    expect(signupButton).toBeInTheDocument();
-    const loginButton = screen.getByText(/or login/i);
-    expect(loginButton).toBeInTheDocument();
 });
 
 
